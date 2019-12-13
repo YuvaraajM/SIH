066f4b0c095768057f27cb4f69d3457b142d7469
YuvaraajM <46411868+YuvaraajM@users.noreply.github.com>
2019-12-13 19:30:25 +0530

    third commit

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 var express = require("express"),
     mongoose = require('mongoose'),
     bodyParser = require('body-parser');
+var LocalStrategy = require("passport-local").Strategy;
+var passport = require("passport");
+var passportLocalMongoose = require("passport-local-mongoose");
+var User = require("./models/user.js");
 
 var app = express();
 
 app.set("view-engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 
 //MongoDB Conection
@@ -20,14 +25,26 @@ const connectDB = async () => {
     console.log("DB connected!.....");
 };
 connectDB();
+//Passport
+app.use(require('express-session')({
+    secret: "hello everyone",
+    resave: false,
+    saveUninitialized: false
+}));
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-var regSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    phoneNo: String
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
-var Reg = new mongoose.model("Reg", regSchema);
+// var regSchema = new mongoose.Schema({
+//     username: String,
+//     password: String,
+//     phoneNo: String
+// });
+
+// var Reg = new mongoose.model("Reg", regSchema);
 
 app.get("/", (req, res) => {
     res.render("index.ejs");
@@ -42,51 +59,49 @@ app.get("/signup", (req, res) => {
 });
 
 app.post("/signup", (req, res) => {
-    Reg.create(req.body.reg,(err,newUser)=>{
-        if(err){
-            console.log("Error in user creation");
-        }else{
-            res.redirect("/");
+    // Reg.create(req.body.reg, (err, newUser) => {
+    //     if (err) {
+    //         console.log("Error in user creation");
+    //     } else {
+    //         res.redirect("/");
+    //     }
+    // });
+    req.body.username
+    req.body.password
+    User.register(new User({ username: req.body.username }), req.body.password, (err, user) => {
+        if (err) {
+            console.log(err);
+            res.redirect("/reg");
         }
+        passport.authenticate("local")(req, res, function () {
+        res.redirect("/login");
+        });
     });
-    // var username = req.body.username;
-    // var password = req.body.password;
-    // Reg.findOne({ username: username })
-    //     .then(userDoc => {
-    //         if (userDoc) {
-    //             return res.redirect("/signup");
-    //         }
-    //         var hashedPassword= bcrypt.hash(password, 12);
-    //             const register = new Reg({
-    //                 username: username,
-    //                 password: hashedPassword
-    //         });
-    //         return register.save();
-    //     });
 });
 
-app.post("/signin", (req, res) => {
-    var username = req.body.username;
-    var password = req.body.password;
+// app.post("/signin", (req, res) => {
+//     var username = req.body.username;
+//     var password = req.body.password;
 
 
-    // console.log(username,password);
-    Reg.findOne({ username:username, password: password })
-        .then(user => {
-            if (!user) {
-                res.redirect("/signin");
-            }
-            else {
-                res.redirect("/");
-            }
-        });
-    // var cursor = collection('regSchema').find({username:this.username ,password:this.password});
-    // if(cursor){
-    //     res.render("/");
-    // }else{
-    //     res.render("/signin");
-    // }
-})
+//     // console.log(username,password);
+//     Reg.findOne({ username: username, password: password })
+//         .then(user => {
+//             if (!user) {
+//                 res.redirect("/signin");
+//             }
+//             else {
+//                 res.redirect("/");
+//             }
+//         });
+// });
+
+app.post("/signin", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+}), function (req,res) {
+    res.redirect("/");
+});
 app.get("/signin", (req, res) => {
     res.render("signin.ejs");
 });
